60cc013ec89decbb1edbb0d9bbbd33b6a3ccc74c
Charles <charlesdcomey@gmail.com>
2023-12-08 18:26:11 -0800

    finished day 1

--- Day 1/calibrater.py
+++ Day 1/calibrater.py
@@ -4,19 +4,29 @@ def read_data(file_name):
     f.close()
     return data
 
-def find_calibration_num(line):
+def find_calibration_num(line, reversed=False):
+    number_words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    if reversed:
+        number_words = [word[::-1] for word in number_words]
+        line = line[::-1]
+
+    for i, char in enumerate(line):
+        # eg 1,2,3
+        if char.isdigit():
+            return int(char)
+        # check n chars past this one to check with n-length word
+        for j, word in enumerate(number_words):
+            if line[i:i+len(word)] == word:
+                return j+1
+    
+    return -1
+
+def find_calibration_nums(line):
     if line == '':
         return 0
     
-    first_digit, last_digit = -1, -1
-    for char in line:
-        if char.isdigit():
-            first_digit = int(char)
-            break
-    for char in reversed(line):
-        if char.isdigit():
-            last_digit = int(char)
-            break
+    first_digit = find_calibration_num(line, False)
+    last_digit = find_calibration_num(line, True)
     
     assert(first_digit >= 0)
     assert(last_digit >= 0)
@@ -33,6 +43,6 @@ def sum_of_list(list):
 data = read_data('data.txt')
 nums = []
 for line in data.split('\n'):
-    nums.append(find_calibration_num(line))
+    nums.append(find_calibration_nums(line))
 
 print('The sum of the calibration numbers is', sum_of_list(nums))
